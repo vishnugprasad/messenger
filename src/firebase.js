d1747754e1d3561efb5ace6853bd5acc109eeaaf
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-02 22:02:28 +0530

    created

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZeCOQ54ukvDVd_yWiFAb1Iqh8NTAIeTE",
+    authDomain: "messagingapp-10126.firebaseapp.com",
+    projectId: "messagingapp-10126",
+    storageBucket: "messagingapp-10126.appspot.com",
+    messagingSenderId: "514034630",
+    appId: "1:514034630:web:8456ae961835e266b940c7",
+    measurementId: "G-7KE631DGC5"
+  };
