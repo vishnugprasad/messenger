439ce809316f49f3de5eddc903ff3b56d9c22921
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-03 08:17:59 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import {Avatar} from "@mui/material"
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import "./Sidebar.css"
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-        <h1>Sidebar</h1>
+        <div className='sidebar-header'>
+          <Avatar></Avatar>
+          <div className='sidebar-headerright'>
+            <DonutLargeIcon></DonutLargeIcon>
+            <ChatIcon></ChatIcon>
+            <MoreVertIcon></MoreVertIcon>
+          </div>
+
+        </div>
+        <div className='sidebar-search'>
+
+        </div>
+
+        <div className='sidebar-char'>
+
+        </div>
     </div>
   )
 }
