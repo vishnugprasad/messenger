d3472bf9fc4549d17e809834a8786fdfc61ab544
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-03 20:02:42 +0530

    sidebar header completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,39 +1,49 @@
-import React from 'react'
-import {Avatar,IconButton} from "@mui/material"
-import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-import ChatIcon from '@mui/icons-material/Chat';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import "./Sidebar.css"
+import React from "react";
+import { Avatar, IconButton } from "@mui/material";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
-        <div className='sidebar-header'>
-          <Avatar></Avatar>
-          <div className='sidebar-headerRight'>
-            <IconButton>
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <Avatar></Avatar>
+        <div className="sidebar-headerRight">
+          <IconButton>
             <DonutLargeIcon></DonutLargeIcon>
-            </IconButton>
+          </IconButton>
 
-            <IconButton>
+          <IconButton>
             <ChatIcon></ChatIcon>
-            </IconButton>
-            <IconButton>
+          </IconButton>
+          <IconButton>
             <MoreVertIcon></MoreVertIcon>
-            </IconButton>
-            
-          </div>
-
+          </IconButton>
         </div>
-        <div className='sidebar-search'>
+      </div>
 
+      <div className="sidebar-search">
+        <div className="sidebar-searchcontainer">
+          <SearchIcon></SearchIcon>
+          <input placeholder="start a new chat" type="text"  />
         </div>
+      </div>
 
-        <div className='sidebar-char'>
-
-        </div>
+      <div className="sidebar-chats">
+        
+        <h1>sidebar chat</h1>
+        <h1>sidebar chat</h1>
+        <h1>sidebar char</h1>
+        <h1>sidebar char</h1>
+        <h1>sidebar char</h1>
+        <h1>sidebar char</h1>
+        <h1>sidebar char</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
