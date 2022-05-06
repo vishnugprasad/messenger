9524e9f0569dc0dbb6c86db054ff73301b1ec241
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-06 12:26:18 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import './App.css';
-import Sidebar from './Sidebar';
-import Chat from "./Chat"
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-     <h1>messenger</h1>
-     <div className = "app-body">
-      <Sidebar></Sidebar>
-      <Chat></Chat>
-     </div>
+      <h1>messenger</h1>
+      <div className="app-body">
+        <Router>
+          <Switch>
+          <Route path = "/app">
+          <Sidebar></Sidebar>
+          <Chat></Chat>
+          </Route>
+            <Route path = "/">
+              <h1>Home Screen</h1>
+            </Route>
+          
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
